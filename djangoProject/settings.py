85ad6697e6c58c2fa989e6b168dfe8fabe2bfd02
Yevhenii <146078178+EvheniiOrza@users.noreply.github.com>
2024-12-13 22:15:24 +0200

    main

--- djangoProject/settings.py
+++ djangoProject/settings.py
@@ -118,13 +118,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = '/static_files/'
-STATICFILES_DIRS = [BASE_DIR / "static_files"]
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 
 LOGIN_REDIRECT_URL = 'event_list'
 LOGOUT_REDIRECT_URL = '/'
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static_files')
 
 
 # Default primary key field type
